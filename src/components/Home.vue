a8e523ae91da9aabd2fba0bf9f3b938b49f870ae
SaurabhApraj <saurabhapraj7@gmail.com>
2020-07-17 20:32:51 +0530

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p v-html="rowHtml"></p>
+    <h3 v-once>{{ name }}</h3>
+    <h3>{{ name }}</h3>
+    <h3>{{ok?"Yes":"No"}}</h3>
+    <!--Ternery Operator-->
+    <h3>Addition: {{10+10}}</h3>
+    <h3>Subtraction: {{b-a}}</h3>
+
+    <button v-bind:disabled="disabledBtn" v-on:click="updateName">Click me</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      name: "Saurabh",
+      a: 10,
+      b: 20,
+      ok: false,
+      rowHtml: "<h1>Hello Saurabh</h1>",
+      disabledBtn: false
+    };
+  },
+  methods: {
+    updateName() {
+      this.ok = !this.ok;
+    }
+  }
+};
+</script>
